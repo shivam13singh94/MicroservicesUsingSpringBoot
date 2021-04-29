606887632d36b7325e3f380c8b883b8451c41709
shivamsingh2k20 <shivamsingh2k20@gmail.com>
2021-04-30 01:00:41 +0530

    initial commit of 3 different microservices

---
+++ MovieCatalogueService/src/main/java/com/java/microservices/MicroservicesUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.java.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ MovieCatalogueService/src/test/java/com/java/microservices/MicroservicesUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
